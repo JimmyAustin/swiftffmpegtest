3c5d70db5fc66a9e831f6794369e421b1966d14c
James Austin <me@jamesaust.in>
2021-01-17 23:33:14 -0800

    Revert "Test"
    This reverts commit 40027238e7aea325865c01accb64c175bdabd2f8.

--- Package.swift
+++ Package.swift
@@ -45,41 +45,41 @@ let package = Package(
     ),
     .binaryTarget(
       name: "libavcodec",
-      url: "./xcframework/libavcodec.zip",
+      url: "https://github.com/JimmyAustin/swiftffmpegtest/blob/master/xcframework/libavcodec.zip?raw=true",
       checksum: "97f406b33946c1ab941af7dd02991e5cc3703779bb018de8ff265eb7a6d236fa"
     ),
     .binaryTarget(
       name: "libavdevice",
       url:
-        "./xcframework/libavdevice.zip",
+        "https://github.com/JimmyAustin/swiftffmpegtest/blob/jamesfork/xcframework/libavdevice.zip?raw=true",
       checksum: "87738dff91380bd23de288e8702f0aa5511d80895c0974daba13dc117bdd599c"
     ),
     .binaryTarget(
       name: "libavfilter",
       url:
-        "./xcframework/libavfilter.zip",
+        "https://github.com/JimmyAustin/swiftffmpegtest/blob/jamesfork/xcframework/libavfilter.zip?raw=true",
       checksum: "cb9b72b3cd923c9b065041cf6a87e164264f16eb282ea338b15bdae6dd5a3d15"
     ),
     .binaryTarget(
       name: "libavformat",
       url:
-        "./xcframework/libavformat.zip",
+        "https://github.com/JimmyAustin/swiftffmpegtest/blob/jamesfork/xcframework/libavformat.zip?raw=true",
       checksum: "9def922d249207ed7be7aebdab7bbd2ff3bc41187dcb0bd77c8e216630407e00"
     ),
     .binaryTarget(
       name: "libavutil",
-      url: "./xcframework/libavutil.zip",
+      url: "https://github.com/JimmyAustin/swiftffmpegtest/blob/jamesfork/xcframework/libavutil.zip?raw=true",
       checksum: "3ca05b51466e4fec332b5c1c607b228a67f4eb7a89e09944d19550474214f4e2"
     ),
     .binaryTarget(
       name: "libswresample",
       url:
-        "./xcframework/libswresample.zip",
+        "https://github.com/JimmyAustin/swiftffmpegtest/blob/jamesfork/xcframework/libswresample.zip?raw=true",
       checksum: "76aa9805c67a040a729eee4599162a982f4bbd2609e7c92b7a3c589159259a7a"
     ),
     .binaryTarget(
       name: "libswscale",
-      url: "./xcframework/libswscale.zip",
+      url: "https://github.com/JimmyAustin/swiftffmpegtest/blob/jamesfork/xcframework/libswscale.zip?raw=true",
       checksum: "b5e47c27f44c6818cc11a41fe43535e7bdb25d171a0b5c4984b52b303c7e53b1"
     ),
     .target(
